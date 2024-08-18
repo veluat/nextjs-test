3e827ef37fe71cc5f040859d066a4f9caf0ca8a5
Julia <veluat@gmail.com>
2024-08-18 08:04:23 +0300

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import {getLayout} from '@/components/Layout/Layout'
+
+const NotFound = () => {
+  return <div>
+    404 NOT FOUND
+  </div>
+}
+
+
+NotFound.getLayout = getLayout
+export default NotFound
